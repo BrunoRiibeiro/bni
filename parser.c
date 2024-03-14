565e5d9779b3def94a04b80caeda1f081d42bdde
Bruno Ribeiro <bbrunoo@icloud.com>
2024-03-14 18:28:51 -0300

    feat: complete :objects and :predicates read

--- parser.c
+++ parser.c
@@ -21,40 +21,51 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
-	Stack domain, problem, p_aux; // stacks for tokenize domain and problem files p_aux is used to parenthesis match in :predicates.
+	Stack domain, problem, p_aux; // Stacks para tokenizar arquivos de domínio e problemas. 'p_aux' é usado para fazer a correspondência de parênteses em ':predicates'.
 	create_stack(&domain, 1000), create_stack(&problem, 1000), create_stack(&p_aux, 100);
-	Item tokend, tokenp; // token variables for domain and problem files.
-	Head hd, hp; // linked lists for string reading in domain and problem files.
+	Item tokend, tokenp; // Variáveis de token para arquivos de domínio e problemas.
+	Head hd, hp; // Linked lists para leitura de strings em arquivos de domínio e problemas.
 	create_list(&hd), create_list(&hp);
 	SymbolTable st;
 	create_st(&st);
-	char *buffer = NULL; // buffer for getline() function on comments ignoring.
-	size_t size_allocated = 0; // size for getline() function on comments ignoring.
+	char *buffer = NULL; // Buffer para a função getline() em comentários ignorados.
+	size_t size_allocated = 0; // Tamanho alocado para a função getline() em comentários ignorados.
+	char obj_sentinel = 0; // Sentinela para objetos que nao tem tipo.
 
 	// Problem parser
 
 	while (fscanf(problem_file, "%c", &tokenp)) {
-		// ignorar os comentário
+		// Ignorar os comentário.
 		if (tokenp == ';') getline(&buffer, &size_allocated, problem_file);
-		// tratamento dos dados
+		// Tratamento dos dados.
 		if (tokenp == ' ' || tokenp == '\n' || tokenp == '\t') {
 			while (top(&problem) != '(')
 				insert_list(&hp, top(&problem)), pop(&problem);
-			// Leitura e tratamento dos dados do "objects" até encontrar ")"
+			// Tratamento dos dados do  bloco "objects" até encontrar ")".
 			if (strcmp_list(&hp, ":objects") == 0) {
 				for ( ; ; ) {
+					// count = how many objects are in one line.
+					// obj = object type string.
 					char count = 0, obj[100];
 					while (fscanf(problem_file, "%s", obj) && obj[0] != '-' && obj[0] != ')')
 						count++;
+					// Verifica se existe um tipo dos objetos lidos.
+					if (obj[0] == '-') obj_sentinel = 1;
+					// Caso nao tenha declarado nenhum tipo de objeto, define objeto padrao como "obj".
+					if (!obj_sentinel) {
+						sprintf(obj, "obj"), add_st(&st, obj, count); 
+						break;
+					}
+					// Bloco de :objects termina com um '\n' antes do ')'.
 					if (obj[0] == ')') break;
-					// Ler até encontrar ")" ou espaco ou quebra de linha ou tab
+					// Ler até encontrar ")" ou espaco ou quebra de linha ou tab.
 					fscanf(problem_file, " %[^)|^ |^\n|^\t]s", obj);
-					// Adiciona ou aumenta a quantidade de repeticoes de um tipo de objeto a tabela de simbolos
+					// Adiciona ou aumenta a quantidade de repeticoes de um tipo de objeto a tabela de simbolos.
 					add_st(&st, obj, count);
 					if (fscanf(problem_file, "%c", &tokenp) && tokenp == ')') break;
 				}
 				print_st(&st);
-				// Pausar a leitura do problema depois de terminar o tratamento de dados do "objects"
+				// Pausar a leitura do problema depois de terminar o tratamento de dados do "objects".
 				break;
 			}
 			free_list(&hp);
@@ -66,29 +77,54 @@ int main(int argc, char *argv[]) {
 	// Domain parser
 	
 	while (fscanf(domain_file, "%c", &tokend) != EOF) {
-		if (tokend == ';') getline(&buffer, &size_allocated, domain_file);
+		// Ignora os comentários.
+			if (tokend == ';') getline(&buffer, &size_allocated, domain_file);
+		// Inicia o tratamento dos dados do domínio.
 		if (tokend == ' ' || tokend == '\n' || tokend == '\t') {
 			while (top(&domain) != '(')
 				insert_list(&hd, top(&domain)),	pop(&domain);
+			// Tratamento dos dados do  bloco "predicates".
 			if (strcmp_list(&hd, ":predicates") == 0) {
 				push(&p_aux, '(');
+				// count = quantos '?' em uma linha.
 				char count = 0;
 				while (fscanf(domain_file, "%c", &tokend) && !is_empty(&p_aux)) {
+					// Ignora comentários dentro do bloco.
 					if (tokend == ';') getline(&buffer, &size_allocated, domain_file);
+					// str = nome do predicado.
+					// buff = string para ser printada no arquivo pela função system.
 					char str[100], buff[100];
+					/* caso:
+					   tokend = '?' -> soma count.
+					   tokend = '-' -> printa [] com a qtd de objetos.
+					   tokend = '(' -> add na pilha de parenteses, printando bool com o nome da variavel.
+					   tokend = ')' -> desempilha, caso esteja vazia encerra o tratamento do bloco :predicates. 
+					   					Caso o obj_sentinel = 0, printa [] com a qtd de objetos pradrao. Printa ';'.
+					*/
 					if (tokend == '?') count++;
-					if (tokend == '(')
+					else if (tokend == '-') {
+						fscanf(domain_file, " %[^)|^ ]s", str);
+						sprintf(buff, "printf '[%lld]' >> domain.c", get_qtd(&st, str));
+						for (int i = 0; i < count; i++)
+							system(buff);
+						count = 0;
+					}
+					else if (tokend == '(') {
 						push(&p_aux, tokend), fscanf(domain_file, "%[^)|^ ]s", str);
+						sprintf(buff, "printf 'bool %s' >> domain.c", str);
+						system(buff);
+					}
 					else if (tokend == ')') {
 						pop(&p_aux);
-						if (!is_empty(&p_aux)) {
-							sprintf(buff, "printf 'int %s' >> domain.c", str);
-							system(buff);
+						if (is_empty(&p_aux)) break;
+						else if (!obj_sentinel) {
+							sprintf(str, "obj"); 
+							sprintf(buff, "printf '[%lld]' >> domain.c", get_qtd(&st, str));
 							for (int i = 0; i < count; i++)
-								system("printf '[]' >> domain.c");
-							system("echo ';' >> domain.c");
+								system(buff);
 							count = 0;
 						}
+						system("echo ';' >> domain.c");
 					}
 				}
 			}
