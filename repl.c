3f451760aa0433d4b6c1eb8f12e78669589d5e3a
Bruno Ribeiro <bbrunoo@icloud.com>
2024-08-02 21:49:37 -0300

    add header + quit and exit

--- repl.c
+++ repl.c
@@ -4,11 +4,20 @@
 #include <readline/history.h>
 #include "pddl.h"
 
-
 typedef struct Map {
     char *name;
     void (*func)();
 }Map;
+
+void printheader() {
+    const char *header =
+		"\033[35m"
+        "#################\n"
+        "#\033[33m bni PDDL REPL\033[35m #\n"
+        "#################\n\n" "\033[0m";
+	printf("\e[1;1H\e[2J");
+    printf("%s", header);
+}
 
 int main(void) {
 	Map functions[] = {
@@ -18,12 +27,14 @@ int main(void) {
 		{"comprardindin3", apply_comprardindin3},
 		{NULL, NULL}
 	};
+	printheader();
 	initialize();
 	char *input;
 	while (1) {
+		printf("Please enter one of the currently available actions:\n");
 		show_actions();
-		input = readline("Digite a acao desejada: ");
-		if (!input) break;
+		input = readline("\n>> ");
+		if (!input || strcmp(input, "quit") == 0 || strcmp(input, "exit") == 0) break;
 		if (*input) add_history(input);
 		for (int i = 0; functions[i].name != NULL; i++)
 			if (strcmp(input, functions[i].name) == 0) {
